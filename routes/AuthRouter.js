5693438108ef76bc5e6a74b8901e80c9c4ac7701
Narrwe <narrwep@gmail.com>
2022-01-14 14:23:47 -0500

    imported auth middleware functions to AuthRouter

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { verifyToken, stripToken } = require('../middleware');
 
 router.get('/', (req, res) => {res.send({ message: 'this is the auth router' })});
 
