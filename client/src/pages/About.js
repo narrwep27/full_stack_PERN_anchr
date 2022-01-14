72dd6090a6fcbd8b9260bfaf6b622c4e949b7c67
Jerome Wy <jeromedaniel.wy@gmail.com>
2022-01-14 13:25:19 -0800

    Created SignIn Page

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -4,6 +4,22 @@ export default function About() {
 	return (
 		<div className='About'>
 			<div className='about-team-div'>
+				<div className='about-project-div'>
+					Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+					accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+					ab illo inventore veritatis et quasi architecto beatae vitae dicta
+					sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+					aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+					qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+					dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+					quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+					aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+					exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+					ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+					ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+					qui dolorem eum fugiat quo voluptas nulla pariatur?
+				</div>
+
 				<div className='about-team-img-div'>
 					<img
 						src='https://media-exp1.licdn.com/dms/image/C4E03AQFCGOerXuChtw/profile-displayphoto-shrink_800_800/0/1630710978360?e=1647475200&v=beta&t=W4EZUap5tQPt0TRD9LH-fZ-Riu0hhRnfcFacnXKV8iQ'
@@ -12,16 +28,65 @@ export default function About() {
 				</div>
 
 				<div className='about-team-details'>
-					<h1>Narrwe Park | Full Stack Developer</h1>
-					<h2>Back-End Guru & Git Master</h2>
+					<h1 className='about-team-h1'>Narrwe Park | Full Stack Developer</h1>
+					<h2 className='about-team-h2'>Back-End Guru & Git Master</h2>
 					<p>
 						Narrwe worked on the Back-End setting up XYZ and also served as the
 						Git Master ensuring the team was properly pushing and pulling at the
 						appropriate times.
 					</p>
+					<p>GitHub | LinkedIn</p>
 				</div>
 			</div>
-			Narrwe Park Dom DeLorenzo Pablo Yocupicio Jerome Wy
+			<div className='about-team-div'>
+				<div className='about-team-img-div'>
+					<img
+						src='https://media-exp1.licdn.com/dms/image/C5603AQFlCgxUpI3Y7w/profile-displayphoto-shrink_800_800/0/1528843672719?e=1647475200&v=beta&t=n9jZ6praFRgZcOpm-TJxNOfJeXm_mBMEP9PD1ZcZDoI'
+						className='about-team-img'
+					/>
+				</div>
+
+				<div className='about-team-details'>
+					<h1 className='about-team-h1'>
+						Dom DeLorenzo | Full Stack Developer
+					</h1>
+					<h2 className='about-team-h2'>Back-End Guru & JavaScript Expert</h2>
+					<p>Dom worked on the Back-End setting up XYZ XYZ XYZ XYZ XYZ XYZ</p>
+					<p>GitHub | LinkedIn</p>
+				</div>
+			</div>
+			<div className='about-team-div'>
+				<div className='about-team-img-div'>
+					<img
+						src='https://ca.slack-edge.com/T0351JZQ0-U02KM3G00HL-588a8e42fe94-512'
+						className='about-team-img'
+					/>
+				</div>
+
+				<div className='about-team-details'>
+					<h1 className='about-team-h1'>
+						Pablo Yocupicio | Full Stack Developer
+					</h1>
+					<h2 className='about-team-h2'>Front-End Pokemon Master</h2>
+					<p>Pablo wants to be the very best that no one ever was.</p>
+					<p>GitHub | LinkedIn</p>
+				</div>
+			</div>
+			<div className='about-team-div'>
+				<div className='about-team-img-div'>
+					<img
+						src='https://media-exp1.licdn.com/dms/image/C4D03AQGTizQ-DzBoUA/profile-displayphoto-shrink_800_800/0/1638998388995?e=1647475200&v=beta&t=6i30NWi8BsOKkmLneCCk_1g-rQfk99MXUSYLmE1C6Og'
+						className='about-team-img'
+					/>
+				</div>
+
+				<div className='about-team-details'>
+					<h1 className='about-team-h1'>Jerome Wy | Full Stack Developer</h1>
+					<h2 className='about-team-h2'>Front-End Summoner</h2>
+					<p>Jerome worked on xyz xyz xyz</p>
+					<p>GitHub | LinkedIn</p>
+				</div>
+			</div>
 		</div>
 	);
 }
