29f1054eaaa9fd8829e0654989cee3905aaf7bea
npark27 <93529617+narrwep27@users.noreply.github.com>
2022-01-16 19:31:43 -0500

    built login controller to return user id, username, email, and new token

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,11 +1,28 @@
-const { reset } = require('nodemon');
+const { User } = require('../models');
 const { hashPassword, comparePassword, createToken } = require('../middleware');
-const middleware = require('../middleware');
 
 const { User } = require('../models');
 
 const Login = async (req, res) => {
   try {
+    const user = await User.findOne({
+      where: { username: req.body.username },
+      raw: true
+    });
+    if (user && (await comparePassword(req.body.password, user.passwordDigest))) {
+      let payload = {
+        id: user.id,
+        username: user.username,
+        email: user.email
+      };
+      let token = createToken(payload);
+      return res.send({
+        user: payload,
+        token: token
+      });
+    } else {
+      res.status(401).send({ status: 'Error', message: 'Unauthorized' });
+    };
   } catch (error) {
     throw error;
   }
@@ -14,7 +31,7 @@ const Login = async (req, res) => {
 const Register = async (req, res) => {
   try {
     const { email, password, username } = req.body;
-    let passwordDigest = await middleware.hashPassword(password);
+    let passwordDigest = await hashPassword(password);
     // let username = name
     const user = await User.create({
       email,
