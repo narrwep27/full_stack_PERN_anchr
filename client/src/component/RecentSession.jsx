f5a29629edc85ef66b9a0975d06be956f393db02
domdelorenzo <delorenzo.breed@gmail.com>
2022-01-19 10:20:46 -0600

    Displaying tag name and time in minutes and seconds on recent history

--- client/src/component/RecentSession.jsx
+++ client/src/component/RecentSession.jsx
@@ -4,14 +4,19 @@ export default function RecentSession(props) {
   let propsToDate = new Date(props.e.createdAt);
   const userDate = propsToDate.toString().slice(4, 16);
   const userTime = propsToDate.toString().slice(16, 24);
-
+  const timeconversion = (t) =>{
+    let minutes = Math.floor(t/60000)
+    let seconds = ((t%60000)/1000).toFixed(0)
+    return minutes+":"+(seconds<10?'0':'')+seconds
+  }
+  let displaytime = timeconversion(props.e.timeSpent)
   return (
     <>
       <tr>
         <td>{userDate}</td>
         <td>{userTime}</td>
-        <td>{props.e.tag_id}</td>
-        <td>{props.e.timeSpent}</td>
+        <td>{props.e.Tag.description}</td>
+        <td>{displaytime}</td>
       </tr>
     </>
   );
