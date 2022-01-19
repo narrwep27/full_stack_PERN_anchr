2be717e138de57fff8091b77fbc6266bc5210c25
Jerome Wy <jeromedaniel.wy@gmail.com>
2022-01-19 10:43:13 -0800

    Added Nav Styling and new fx in SessionController

--- controllers/SessionController.js
+++ controllers/SessionController.js
@@ -1,75 +1,87 @@
 const { Session, Tag } = require('../models');
 
 const GetAllSessions = async (req, res) => {
-  try {
-    const sessions = await Session.findAll();
-    res.send(sessions);
-  } catch (error) {
-    throw error;
-  }
+	try {
+		const sessions = await Session.findAll();
+		res.send(sessions);
+	} catch (error) {
+		throw error;
+	}
 };
 
 const GetSessionByID = async (req, res) => {
-  try {
-    let id = parseInt(req.params.session_id);
-    const session = await Session.findByPk(id);
-    res.send(session);
-  } catch (error) {
-    throw error;
-  }
+	try {
+		let id = parseInt(req.params.session_id);
+		const session = await Session.findByPk(id);
+		res.send(session);
+	} catch (error) {
+		throw error;
+	}
 };
 
 const GetSessionByUserId = async (req, res) => {
-  try {
-    const sessions = await Session.findAll({ 
-      where: {user_id: req.params.user_id},
-      include: Tag,
-      order: ['id']
-    });
-    res.send(sessions);
-  } catch (error) {
-    throw error;
-  };
+	try {
+		const sessions = await Session.findAll({
+			where: { user_id: req.params.user_id },
+			include: Tag,
+			order: ['id'],
+		});
+		res.send(sessions);
+	} catch (error) {
+		throw error;
+	}
+};
+
+const TotalAmounts = async (req, res) => {
+	try {
+		const sessions = await sequelize.query(
+			`SELECT description, SUM(sessions."timeSpent") AS "Total_Time" FROM sessions INNER JOIN tags ON (sessions.tag_id = tags.id) WHERE sessions."user_id" = ${req.params.user_id} GROUP BY description`
+		);
+		res.send(sessions[0]);
+	} catch (error) {
+		throw error;
+	}
 };
 
 const CreateSession = async (req, res) => {
-  try {
-    let session = await Session.create(req.body);
-    res.send(session);
-  } catch (error) {
-    console.log(req.body);
-    throw error;
-  }
+	try {
+		let session = await Session.create(req.body);
+		res.send(session);
+	} catch (error) {
+		console.log(req.body);
+		throw error;
+	}
 };
 
 const UpdateSession = async (req, res) => {
-  try {
-    let id = parseInt(req.params.session_id);
-    let sessionUpdate = await Session.update(req.body, {
-      where: { id: id },
-      returning: true
-    });
-    res.send(sessionUpdate);
-  } catch (error) {
-    throw error;
-  }
+	try {
+		let id = parseInt(req.params.session_id);
+		let sessionUpdate = await Session.update(req.body, {
+			where: { id: id },
+			returning: true,
+		});
+		res.send(sessionUpdate);
+	} catch (error) {
+		throw error;
+	}
 };
 
 const DeleteSession = async (req, res) => {
-  try {
-    let id = parseInt(req.params.session_id);
-    await Session.destroy({ where: { id: id } });
-    res.send({ message: `Session ID ${id} has been deleted.` });
-  } catch (error) {
-    throw error;
-  }
+	try {
+		let id = parseInt(req.params.session_id);
+		await Session.destroy({ where: { id: id } });
+		res.send({ message: `Session ID ${id} has been deleted.` });
+	} catch (error) {
+		throw error;
+	}
 };
 
 module.exports = {
-  CreateSession,
-  GetAllSessions,
-  GetSessionByID,
-  GetSessionByUserId,
-  UpdateSession,
-  DeleteSession
+	CreateSession,
+	GetAllSessions,
+	GetSessionByID,
+	GetSessionByUserId,
+	UpdateSession,
+	DeleteSession,
+	TotalAmounts,
 };
--- routes/SessionRouter.js
+++ routes/SessionRouter.js
@@ -2,16 +2,17 @@ const Router = require('express').Router();
 const controller = require('../controllers/SessionController');
 const { stripToken, verifyToken } = require('../middleware');
 
+Router.get('/getsessions/:user_id', controller.TotalAmounts);
 Router.get('/', controller.GetAllSessions);
 Router.get('/:session_id', controller.GetSessionByID);
 Router.get(`/user/:user_id`, controller.GetSessionByUserId);
 Router.post('/new/', stripToken, verifyToken, controller.CreateSession);
 Router.put('/:session_id', stripToken, verifyToken, controller.UpdateSession);
 Router.delete(
-  '/:session_id',
-  stripToken,
-  verifyToken,
-  controller.DeleteSession
+	'/:session_id',
+	stripToken,
+	verifyToken,
+	controller.DeleteSession
 );
 
 module.exports = Router;
