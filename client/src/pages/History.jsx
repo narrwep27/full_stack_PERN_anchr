49b4e9ebb487c0679d36c996a11101667e7d8098
Jerome Wy <jeromedaniel.wy@gmail.com>
2022-01-20 10:56:35 -0800

    Committing prior to pulling staging changes

--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -1,46 +1,53 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import { LoadUserById } from '../services/User';
 import { LoadUserSessions } from '../services/Session';
-import HistorySession from "../component/HistorySession";
-import toast from 'react-hot-toast'
+import HistorySession from '../component/HistorySession';
+import toast from 'react-hot-toast';
 
 function History(props) {
-  const [user, setUser] = useState({});
-  const [sessions, setSessions] = useState([]);
+	const [user, setUser] = useState({});
+	const [sessions, setSessions] = useState([]);
 
-  const getUser = async () => {
-    let currentUser = await LoadUserById(props.user.id);
-    setUser(currentUser);
-  };
-  const getSessions = async () => {
+	const getUser = async () => {
+		let currentUser = await LoadUserById(props.user.id);
+		setUser(currentUser);
+	};
+	const getSessions = async () => {
 		const userSessions = await LoadUserSessions(localStorage.getItem('id'));
-    let datedSess = userSessions.map((index) => {
-      let date = new Date(index.createdAt)
-      let newDate = date.toString().slice(4,15);
-      return {...index, date: newDate};
-    })
+		let datedSess = userSessions.map((index) => {
+			let date = new Date(index.createdAt);
+			let newDate = date.toString().slice(4, 15);
+			return { ...index, date: newDate };
+		});
 		setSessions(datedSess);
 	};
 
-  useEffect(async () => {
-    await getUser();
-    getSessions();
-  }, []);
+	useEffect(async () => {
+		await getUser();
+		getSessions();
+	}, []);
 
-  const deleteNotify = () => toast("Session Deleted", {style: { background: 'red', color: 'white'}});
+	const deleteNotify = () =>
+		toast('Session Deleted', { style: { background: 'red', color: 'white' } });
 
-  return (
-    <div className="history-div">
-      <div className="history-grid">
-        <p className="history-date-col"><b>Date</b></p>
+	return (
+		<div className='history-div'>
+			<div className='history-grid'>
+				{/* <p className="history-date-col"><b>Date</b></p>
         <p className="history-tag-col"><b>Tag</b></p>
-        <p className="history-time-col"><b>Time Focused</b></p>
-        {sessions.map((index) => (
-          <HistorySession key={index.id} session={index} allTags={user.Tags} getSessions={getSessions} deleteNotify={deleteNotify}/>
-        ))}
-      </div>
-    </div>
-  );
+        <p className="history-time-col"><b>Time Focused</b></p> */}
+				{sessions.map((index) => (
+					<HistorySession
+						key={index.id}
+						session={index}
+						allTags={user.Tags}
+						getSessions={getSessions}
+						deleteNotify={deleteNotify}
+					/>
+				))}
+			</div>
+		</div>
+	);
 }
 
 export default History;
