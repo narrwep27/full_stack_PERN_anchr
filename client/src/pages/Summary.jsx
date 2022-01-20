fca150c3809e1b72782b401f613d38fc0a5dfffa
Jerome Wy <jeromedaniel.wy@gmail.com>
2022-01-20 13:32:55 -0800

    Summary page formatting fixed

--- client/src/pages/Summary.jsx
+++ client/src/pages/Summary.jsx
@@ -1,67 +1,85 @@
-import React, { useEffect, useState } from "react";
-import { TotalAmount } from "../services/Session";
-import { PieChart, Pie, Tooltip, Cell } from "recharts";
-import { randomColor } from "randomcolor";
+import React, { useEffect, useState } from 'react';
+import { TotalAmount } from '../services/Session';
+import { PieChart, Pie, Tooltip, Cell } from 'recharts';
+import { randomColor } from 'randomcolor';
 
 export default function Summary(props) {
-  const sessions = JSON.parse(localStorage.getItem("sessions"));
-  const id = localStorage.getItem("id");
-  const [allTime, setAllTime] = useState([0]);
+	const sessions = JSON.parse(localStorage.getItem('sessions'));
+	const id = localStorage.getItem('id');
+	const [allTime, setAllTime] = useState([0]);
 
-  const timeArray = [0];
+	const timeArray = [0];
 
-  const sumNumbers = () => {
-    sessions.map((e) => {
-      return timeArray.push(e.timeSpent);
-    });
-  };
+	const sumNumbers = () => {
+		sessions.map((e) => {
+			return timeArray.push(e.timeSpent);
+		});
+	};
 
-  sumNumbers();
+	sumNumbers();
 
-  useEffect(() => {
-    const getTotal = async () => {
-      const res = await TotalAmount(id);
-      setAllTime(res);
-    };
-    getTotal();
-  }, []);
+	useEffect(() => {
+		const getTotal = async () => {
+			const res = await TotalAmount(id);
+			setAllTime(res);
+		};
+		getTotal();
+	}, []);
 
-  const sum = timeArray.reduce((a, r) => a + r);
+	const sum = timeArray.reduce((a, r) => a + r);
 
-  const timeconversion = (t) => {
-    let minutes = Math.floor(t / 60000);
-    let seconds = ((t % 60000) / 1000).toFixed(0);
-    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
-  };
+	const timeconversion = (t) => {
+		let minutes = Math.floor(t / 60000);
+		let seconds = ((t % 60000) / 1000).toFixed(0);
+		return minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+	};
 
-  const data = allTime.map((e, i) => {
-    return { description: e.description, total_time: Math.round((parseInt(e.total_time) / sum) * 10000) / 100, fill: randomColor() };
-  });
+	const data = allTime.map((e, i) => {
+		return {
+			description: e.description,
+			total_time: Math.round((parseInt(e.total_time) / sum) * 10000) / 100,
+			fill: randomColor(),
+		};
+	});
 
-  const totalTime = timeconversion(sum);
-  console.log(data)
+	const totalTime = timeconversion(sum);
+	console.log(data);
 
-  return (
-    <div className="Summary">
-      <div className="summary-each-item">
-        <h1>Summary</h1>
-        {allTime.map((e, i) => (
-          <div key={i}>
-            {e.description}: {timeconversion(e.total_time)}
-          </div>
-        ))}
-        <h3>Total time spent: {totalTime}</h3>
-      </div>
+	return (
+		<div className='Summary'>
+			<div className='summary-each-item'>
+				<h1>Summary:</h1>
+				{allTime.map((e, i) => (
+					<div key={i}>
+						<div className='summary-items-container'>
+							<div className='summary-items'>{e.description}:</div>
+							<div className='summary-items'>
+								{timeconversion(e.total_time)}
+							</div>
+						</div>
+					</div>
+				))}
+				<span className='summary-total-timespent-text'>
+					Total time spent: <b>{totalTime}</b>
+				</span>
+			</div>
 
-      <div className="piechart-div">
-        <PieChart width={500} height={500}>
-          <Pie paddingAngle={1} innerRadius={150} outerRadius={250} nameKey="description" dataKey="total_time" isAnimationActive={true} data={data} ></Pie>
-          {data.map((entry, index) => (
-            <Cell fill="#36078E" />
-          ))}
-          <Tooltip label="Total Time" />
-        </PieChart>
-      </div>
-    </div>
-  );
+			<div className='piechart-div'>
+				<PieChart width={500} height={500}>
+					<Pie
+						paddingAngle={1}
+						innerRadius={150}
+						outerRadius={250}
+						nameKey='description'
+						dataKey='total_time'
+						isAnimationActive={true}
+						data={data}></Pie>
+					{data.map((entry, index) => (
+						<Cell fill='#36078E' />
+					))}
+					<Tooltip label='Total Time' />
+				</PieChart>
+			</div>
+		</div>
+	);
 }
