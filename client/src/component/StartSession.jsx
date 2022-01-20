66c971d41b6624257ff77e8b153b0ac7b35ad956
domdelorenzo <delorenzo.breed@gmail.com>
2022-01-20 14:38:32 -0600

    Timer cannot start if tag or time are not chosen

--- client/src/component/StartSession.jsx
+++ client/src/component/StartSession.jsx
@@ -3,12 +3,14 @@ import { AddTag } from '../services/Tag';
 import TagButton from './TagButton';
 
 export default function StartSession(props) {
-  const [tagInput, setTagInput] = useState(true);
-  // const [selectorValue, setSelectorValue] = useState(null);
+  const [tagInput, setTagInput] = useState(false);
   const handleSession = (e) => {
     e.preventDefault();
-    props.setSession(false);
-    props.setStart(true);
+    if ((props.sessionObject.tagId!=='') && (props.sessionObject.timeSpent!==0)){
+      props.setSession(false);
+      props.setStart(true);
+    }
+  
   };
   const postNewTag = async (e) => {
     e.preventDefault()
@@ -29,56 +31,21 @@ export default function StartSession(props) {
   console.log(props.sessionObject)
   return (
     <div>
-    <div className='StartSession'>      
-      {/* <form onSubmit={handleSession}>
-        
-        <select form="timerform" id="tagDropDown" onChange={tagDropdownHandler} value={selectorValue} required>
-          <option value="" id="newTag">Add new tag...</option>
-          {props.userTags.map((e, i) => (
-            <option key={i} value={e.id}>{e.description}</option>
-          ))}
-          
-        </select>
-      </form>
-      {tagInput ? 
-        <form onSubmit={postNewTag}>
-          <button type="submit">Add</button>
-          <input  value={selectorValue} name="description" onChange={props.tagChange} placeholder="Enter tag name..."></input> 
-        </form>
-        :
-        <div></div>
-        }
-      {tagInput ? 
-        <div></div>
-        :
-        <form onSubmit={handleSession} id="timerform">
-          <button  type="submit">Start Session</button>
-          <input type="number" name="timeSpent" onChange={props.handleChange} placeholder="Enter session time in minutes"></input>
-        </form>} */}
-      <form onSubmit={handleSession} id="timerform">
-        <button className="start-session-btn" type="submit">Start Session</button>
-        <input type="number" className="start-session-minutes-form" name="timeSpent" onChange={props.handleChange} placeholder="Enter session time in minutes"></input>
-      </form>
-      <TagButton
+    <div className='StartSession'>
+    <TagButton
         userTags={props.userTags}
         tagDropdownHandler={tagDropdownHandler}
         tagInput={tagInput}
         tagChange={props.tagChange}
         postNewTag={postNewTag}
-      />
-    </div>
-     {/* {tagInput ? 
-      <form onSubmit={postNewTag}>
-        <button type="submit">Add</button>
-        <input  
-        // value={selectorValue} 
-        name="description" 
-        onChange={props.tagChange} 
-        placeholder="Enter tag name..."></input> 
+        sessionObject={props.sessionObject}
+      />      
+      <form onSubmit={handleSession} id="timerform">
+        <button className="start-session-btn" type="submit">Start Session</button>
+        <input type="number" className="start-session-minutes-form" name="timeSpent" onChange={props.handleChange} placeholder="Enter session time in minutes"></input>
       </form>
-      :
-      <div></div>
-      } */}
+
+    </div>
       </div>
   );
 }
--- client/src/component/TagButton.jsx
+++ client/src/component/TagButton.jsx
@@ -1,12 +1,37 @@
 import React, {useEffect, useState} from 'react'
+import {GetTagByTagId} from '../services/Tag'
 
 const TagButton = (props) => {
   const [openDrop, setOpenDrop] = useState(false)
+  const [currentTag, setCurrentTag]=useState('')
   const drop = React.useRef(null)
- 
+//show current tag name on button
+//create constant to change value depending on selected tag
+let buttonDisplay = ''
+
+const getCurrentTag = async () => {
+  const result = await GetTagByTagId(props.sessionObject.tagId)
+  setCurrentTag(result.description)
+  return result
+}
+
+  getCurrentTag()
+
+
+
+const displayTag = () => {
+  let tag = props.sessionObject.tagId
+  if (tag===''){
+    buttonDisplay='Choose Tag'
+  } else {
+    buttonDisplay=`${currentTag}`
+  }
+}
+displayTag()
+
   return (
     <div ref={drop} className="tag-dropdown-container">
-      <button className="start-session-btn" onClick={()=>setOpenDrop(openDrop=>!openDrop)}>Choose Tag</button>
+      <button className="start-session-btn" onClick={()=>setOpenDrop(openDrop=>!openDrop)}>{buttonDisplay}</button>
       <div className="tag-dropdown-option-list">
       {openDrop && props.userTags.map((e, i) => (
             <div className="start-session-tags-dropdown" onClick={props.tagDropdownHandler} key={i} id={e.id}>{e.description}</div>
