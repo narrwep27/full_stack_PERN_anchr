7d5799f747fcf88ed5d167ba22c3135173905bb4
Pablo Yocupicio <Pabloey@yahoo.com>
2022-01-19 17:18:18 -0700

    Time converted in Summary page to minutes. Fixed nav and userhome

--- client/src/component/Nav.jsx
+++ client/src/component/Nav.jsx
@@ -1,36 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function Nav(props) {
-	const logout = (e) => {
-		props.setAuth(false);
-		props.setUser(null);
-		localStorage.clear();
-	};
+  const logout = (e) => {
+    props.setAuth(false);
+    props.setUser(null);
+    localStorage.clear();
+  };
 
-	return (
-		<div className='Nav'>
-			<div class='nav-items-container'>
-				<span className='nav-items'>
-					<Link to='/'>Home</Link>
-				</span>
-				<span className='nav-items'>
-					<Link to='/history'>History</Link>
-				</span>
-				<span className='nav-items'>
-					<Link to='/summary'>Summary</Link>
-				</span>
-				<span className='nav-items'>
-					<Link to='/about'>About</Link>
-				</span>
-
-				{props.setUser ? (
-					<span className='nav-items-items' onClick={logout}>
-						<Link to='/'>Log Out</Link>
-					</span>
-				) : (
-					''
-				)}
-			</div>
-		</div>
-	);
+  return (
+    <div className="Nav">
+      <div class="nav-items-container">
+        <span className="nav-items">
+          <Link to="/">Home</Link>
+        </span>
+        <span className="nav-items">
+          <Link to="/history">History</Link>
+        </span>
+        <span className="nav-items">
+          <Link to="/summary">Summary</Link>
+        </span>
+        <span className="nav-items">
+          <Link to="/about">About</Link>
+        </span>
+        <span className="nav-items-items" onClick={logout}>
+          <Link to="/">Log Out</Link>
+        </span>
+      </div>
+    </div>
+  );
 }
--- client/src/pages/Summary.jsx
+++ client/src/pages/Summary.jsx
@@ -1,6 +1,6 @@
 export default function Summary(props) {
-
   const sessions = JSON.parse(localStorage.getItem("sessions"));
+
   const timeArray = [];
 
   const sumNumbers = () => {
@@ -12,15 +12,23 @@ export default function Summary(props) {
 
   const sum = timeArray.reduce((a, r) => a + r);
 
+  const timeconversion = (t) => {
+    let minutes = Math.floor(t / 60000);
+    let seconds = ((t % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  };
+
+  const totalTime = timeconversion(sum);
+
   return (
     <div>
       <h1>Summary</h1>
       {sessions.map((e, i) => (
         <p key={i}>
-          {e.Tag.description}: {e.timeSpent}
+          {e.Tag.description}: {timeconversion(e.timeSpent)}
         </p>
       ))}
-      <h3>Total time spent: {sum}</h3>
+      <h3>Total time spent: {totalTime}</h3>
     </div>
   );
 }
