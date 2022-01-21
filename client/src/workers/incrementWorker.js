9b9e1b5eadd92a5b21055f4e7bafba0d3585e8aa
domdelorenzo <delorenzo.breed@gmail.com>
2022-01-21 11:18:49 -0600

    Added webworker components

---
+++ client/src/workers/incrementWorker.js
@@ -0,0 +1,22 @@
+export default () => {
+  interval = setInterval(()=> {
+    if  
+  })
+  // let interval = null;
+  // if (start) {
+  //   interval = setInterval(() => {
+  //     if (time > 0) {
+  //       setTime((previousTime) => previousTime - 10);
+  //     } else {
+  //       logSession();
+  //       setStart(false);
+  //     }
+  //   }, 10);
+  // } else {
+  //   setStart(false);
+  //   clearInterval(interval);
+  // }
+  // return () => {
+  //   clearInterval(interval);
+  // };
+};
